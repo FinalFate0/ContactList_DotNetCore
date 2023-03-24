f38f5004bce1a1133f781cc895c4bffa920eba0c
Tymoteusz Urbanowicz <finalfate000@gmail.com>
2023-03-24 22:45:55 +0100

    finish contacts interface; update demonstration html

--- angularapp/src/app/app.component.ts
+++ angularapp/src/app/app.component.ts
@@ -18,8 +18,21 @@ export class AppComponent {
   title = 'angularapp';
 }
 
+interface Category {
+  id: number;
+  name: string;
+}
+
 interface Contacts {
-  id: bigint;
+  id: number;
   firstName: string;
   lastName: string;
+  email: string;
+  password: string;
+  categoryId: number;
+  category: { [key: string]: Category };
+  subcategoryId: number;
+  subcategory?: { [key: string]: Category };
+  phoneNumber: string;
+  dateOfBirth: string;
 }
